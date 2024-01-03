028d900a8a595d2945b331767553101f63783382
Shahboz <sharafzodashahboz@gmail.com>
2024-01-03 12:07:11 +0500

    models registered for admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from core.models import Restourant, Rating, Sale
 
-# Register your models here.
+admin.site.register(Restourant)
+admin.site.register(Rating)
+admin.site.register(Sale)
+
