3f1a6272ee2e3b7a3f82945d34f018f051bc335a
Shahboz <sharafzodashahboz@gmail.com>
2024-01-06 13:37:02 +0500

    ways to create a record

--- core/scripts/orm_script.py
+++ core/scripts/orm_script.py
@@ -1,13 +1,36 @@
 from core.models import Restourant, Sale
 from django.utils import timezone
+from django.db import connection
 def run():
     
-    restaurant = Restourant()
-    restaurant.name = 'My Italian Restaurant'
-    restaurant.latitude = 50.2
-    restaurant.longitude = 40.5
-    restaurant.date_opened = timezone.now()
-    restaurant.restourant_type = Restourant.TypeChoices.ITALIAN
-    restaurant.save()
-    
+    # in order to create a record you do like this
 
+    # restaurant = Restourant()
+    # restaurant.name = 'My Italian Restaurant'
+    # restaurant.latitude = 50.2
+    # restaurant.longitude = 40.5
+    # restaurant.date_opened = timezone.now()
+    # restaurant.restourant_type = Restourant.TypeChoices.ITALIAN
+    # restaurant.save()
+
+    # to pull out the records you do 
+
+    # restaurant = Restourant.objects.all()
+    # print(restaurant)
+    #print(connection.queries)
+
+    # the second way to create objects in Django 
+    # Restourant.objects.create(
+    #     name='Pizza shop',
+    #     date_opened = timezone.now(),
+    #     restourant_type = Restourant.TypeChoices.ITALIAN,
+    #     latitude = 30.8,
+    #     longitude = 22.3 
+    # )
+    # print(connection.queries)
+
+    # to get the count of restaurants you can do 
+    print(Restourant.objects.count())
+    print(connection.queries)
+
+    # we have also last method, first method
