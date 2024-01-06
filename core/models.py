0685f3544e6a574e8025b55fc3d0bcd610b556ba
Shahboz <sharafzodashahboz@gmail.com>
2024-01-06 16:31:56 +0500

    related name added

--- core/models.py
+++ core/models.py
@@ -29,14 +29,14 @@ class Restourant(models.Model):
 
 class Rating(models.Model):
     user = models.ForeignKey(User, on_delete = models.CASCADE)
-    restaurant = models.ForeignKey(Restourant, on_delete=models.CASCADE)
+    restaurant = models.ForeignKey(Restourant, on_delete=models.CASCADE, related_name='ratings')
     rating = models.PositiveSmallIntegerField()
 
     def __str__(self):
         return f'Rating: {self.rating}'
     
 class Sale(models.Model):
-    restaurant = models.ForeignKey(Restourant, on_delete = models.SET_NULL, null=True,blank=True)
+    restaurant = models.ForeignKey(Restourant, on_delete = models.SET_NULL, null=True,blank=True, related_name='sales')
     income = models.DecimalField(max_digits=8,decimal_places=2)
     datetime = models.DateTimeField()
     
