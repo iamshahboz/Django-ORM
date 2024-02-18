b6df55381c07bd40293833a10a3db3aaeafea8ef
Shahboz <sharafzodashahboz@gmail.com>
2024-02-18 23:19:04 +0500

    type error fixed

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from core.models import Restourant, Rating, Sale
+from core.models import Restaurant, Rating, Sale
 
-admin.site.register(Restourant)
+admin.site.register(Restaurant)
 admin.site.register(Rating)
 admin.site.register(Sale)
 
